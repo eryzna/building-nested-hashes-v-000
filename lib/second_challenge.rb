458ea541eaa59f5025af9cc6a0ab88a986bb7196
Elana Ryznar <eryzna@gmail.com>
2018-05-21 21:57:31 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,8 +5,8 @@ def second_challenge
       :matriarch
       :hero
       :hero_friends
-      #your key/value pairs here!
-      },
+    }
+      #your key/value pairs here!}
     :capulet => {
       :patriarch
       :matriarch
